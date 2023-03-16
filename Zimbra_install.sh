c1c322f9e9d46cb6f0edcbd33397d36ab32fce7d
Aashish Chaurasiya <aashishchaurasiya999@gmail.com>
2023-03-16 20:47:12 +0530

    ZIMBRA_INSTALL UBUNTU 20.04

--- Zimbra_install.sh
+++ Zimbra_install.sh
@@ -11,7 +11,10 @@ wget https://files.zimbra.com/downloads/8.8.15_GA/zcs-8.8.15_GA_4179.UBUNTU20_64
 echo EXTRACTING TAR
 
 tar -xf zcs-8.8.15_GA_4179.UBUNTU20_64.20211118033954.tgz 
-cd zcs-8.8.15_GA_4179.UBUNTU20_64.20211118033954
+
 
 #
-echo Now run ./install.sh
+echo " ##  NOW RUN
+cd zcs-8.8.15_GA_4179.UBUNTU20_64.20211118033954
+./install.sh
+""
